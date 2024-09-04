d0c8d35847c60f72f31b7cacb4b3231bad088f7e
mardokh <ahoura.mardokh@gmail.com>
2024-09-04 21:16:59 +0100

    stage test node

--- routes/endPoint.js
+++ routes/endPoint.js
@@ -1,22 +1,10 @@
 // import modules
-const express = require('express');
-const path = require('path')
+const express = require('express')
 const controller = require('../controllers/user.controller.js')
 const middleware = require('../middleware/jwt.auth.js')
 
-
 // Initialize express router
 const endPoint = express.Router()
-
-// Main page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'))
-})
-
-// Dashboard page 
-router.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'dashboard.html'))
-})
 
 router.get('/api/user', middleware.checkToken, controller.getUser)
 
